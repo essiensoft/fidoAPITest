fe02883c747a03304850a6c409b4894a8ef491e7
essiensoft <atofelixessien@gmail.com>
2025-02-13 22:42:44 +0000

    Consolidate VideoGameRoutes into Routes and rename class

--- src/main/java/org/fidoTest/api/endpoints/Routes.java
+++ src/main/java/org/fidoTest/api/endpoints/Routes.java
@@ -1,13 +1,17 @@
 package org.fidoTest.api.endpoints;
 
-public class VideoGameRoutes {
+public class Routes {
     public static String BASE_URL = "https://www.videogamedb.uk/";
 
+    //    Api Authentication Controller
+    public static String POST_URL = BASE_URL+"/api/authenticate";
     //    Api Video Games Controller
     public static String GET_URL = BASE_URL+"/api/videogame";
     public static String POST_URL_GAME = BASE_URL+"/api/videogame";
     public static String GET_URL_GAME = BASE_URL+"api/videogame/{id}";
     public static String PUT_URL_GAME = BASE_URL+"api/videogame/{id}";
     public static String DELETE_URL_GAME = BASE_URL+"api/videogame/{id}";
+
+
 
 }
